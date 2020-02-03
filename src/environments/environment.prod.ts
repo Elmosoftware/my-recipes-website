d916de73963eadf4bf6fb175a3d9dfc8a9af3cbe
Juan Carlos Garcia <juancarlosgarcia_arg@hotmail.com>
2020-02-03 08:24:09 -0300

    Fixed #110 Version update to v1.0.4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   appName: "Mi Cocina",
-  appVersion: "1.0.3",
+  appVersion: "1.0.4",
   appURL: "https://micocina.herokuapp.com",
   apiURL: "https://micocina-api.herokuapp.com/api/",
   apiManagementEndpoint: "management/",
